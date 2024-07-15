f1a804441ecf4174f9d85c944db86642ada16145
HCarlos <r0@tecnointel.mx>
2024-07-15 22:56:48 +0000

    refugios - A38 | L11.6 Production

--- resources/js/Pages/Refugios/Form.vue
+++ resources/js/Pages/Refugios/Form.vue
@@ -184,12 +184,12 @@ const showImg = (e) => {
                         <path stroke-linecap="round" stroke-linejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
                     </svg>
                 </InputGroup>
-                <InputGroup v-model="form.longitud" :errors="form.errors.longitud" :placeholder="'Longitud'"  :type="'text'" :class-especial="'em15 bg-yellow-50'">
+                <InputGroup v-model="form.longitud" :errors="form.errors.longitud" :placeholder="'Longitud'" :type="'text'" :class-especial="'em15 bg-yellow-50'">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
                     </svg>
                 </InputGroup>
-                <InputGroup v-model="form.poligono" :errors="form.errors.poligono" :placeholder="'Polígono'"  :type="'text'" >
+                <InputGroup v-model="form.poligono" :errors="form.errors.poligono" :placeholder="'Polígono'" :type="'text'" >
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
                     </svg>
